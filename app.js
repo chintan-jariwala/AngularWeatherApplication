218fd0baa5c68aa3714deda4ec53e95dcd7dc250
Chintan Jariwala <chintanallstarz@gmail.com>
2016-06-05 19:15:44 -0700

    Initial Commit

---
+++ app.js
@@ -0,0 +1,34 @@
+(function(){
+
+  'use strict';
+
+  var app = angular.module('weatherApp', []);
+
+  app.controller('MainCtrl', ['$scope','$http',function($scope,$http){
+    $scope.APIKEY = "dd1e70fef626370ab142309e1ea51374";
+    $scope.test = 'My Weather Application';
+    $scope.city = "";
+    $scope.details = "";
+    $scope.places = [];
+    function weatherData(city, weather){
+      this.city = city;
+      this.weather = weather;
+    }
+
+    $scope.searchWeather= function(){
+      console.log($scope.city);
+      $http.get("http://api.openweathermap.org/data/2.5/weather?q="+$scope.city+"&&appid="+$scope.APIKEY)
+    .then(
+      function(response){
+        $scope.details = response.data;
+        console.log($scope.details.weather[0].description);
+        $scope.places.push(new weatherData($scope.city,$scope.details.weather[0].description));
+      });
+
+    }
+
+
+
+  }]);
+
+})()
